06fad0d5b222e6b31398dbffd940a03fcc1d707f
XIIIvs <xiii_1994@wp.pl>
2017-10-25 13:10:54 +0200

    add sharpen

--- OpenCV/OpenCV.cpp
+++ OpenCV/OpenCV.cpp
@@ -7,6 +7,35 @@
 #include <string>
 using namespace cv;
 using namespace std;
+
+void Sharpen(const Mat& myImage, Mat& Result)
+{
+	CV_Assert(myImage.depth() == CV_8U);  // accept only uchar images
+
+	Result.create(myImage.size(), myImage.type());
+	const int nChannels = myImage.channels();
+
+	for (int j = 1; j < myImage.rows - 1; ++j)
+	{
+		const uchar* previous = myImage.ptr<uchar>(j - 1);
+		const uchar* current = myImage.ptr<uchar>(j);
+		const uchar* next = myImage.ptr<uchar>(j + 1);
+
+		uchar* output = Result.ptr<uchar>(j);
+
+		for (int i = nChannels; i < nChannels * (myImage.cols - 1); ++i)
+		{
+			*output++ = saturate_cast<uchar>(5 * current[i]
+				- current[i - nChannels] - current[i + nChannels] - previous[i] - next[i]);
+		}
+	}
+
+	Result.row(0).setTo(Scalar(0));
+	Result.row(Result.rows - 1).setTo(Scalar(0));
+	Result.col(0).setTo(Scalar(0));
+	Result.col(Result.cols - 1).setTo(Scalar(0));
+}
+
 int main(int argc, char** argv)
 {
 	if (argc != 2)
@@ -39,20 +68,35 @@ int main(int argc, char** argv)
 	imshow(window_name_1, image);
 	imshow(window_name_2, gray_image);
 
-	String window_name_3r = "Red image";
-	String window_name_3g = "Green image";
-	String window_name_3b = "Blue image";
+	String window_name_r = "Red image";
+	String window_name_g = "Green image";
+	String window_name_b = "Blue image";
 
-	namedWindow(window_name_3r, WINDOW_AUTOSIZE);
-	namedWindow(window_name_3g, WINDOW_AUTOSIZE);
-	namedWindow(window_name_3b, WINDOW_AUTOSIZE);
+	namedWindow(window_name_r, WINDOW_AUTOSIZE);
+	namedWindow(window_name_g, WINDOW_AUTOSIZE);
+	namedWindow(window_name_b, WINDOW_AUTOSIZE);
 
 	Mat color_layers[3];
 	split(image, color_layers);
 
-	imshow(window_name_3b, color_layers[0]);
-	imshow(window_name_3g, color_layers[1]);
-	imshow(window_name_3r, color_layers[2]);
+	imshow(window_name_b, color_layers[0]);
+	imshow(window_name_g, color_layers[1]);
+	imshow(window_name_r, color_layers[2]);
+
+	waitKey(0);
+
+	Mat sharpen;
+
+	String window_name_sharp = "Sharp image";
+	namedWindow(window_name_sharp, WINDOW_AUTOSIZE);
+
+	Sharpen(image, sharpen);
+
+	imshow(window_name_sharp, sharpen);
+
+	waitKey(0);
+
+
 
 	waitKey(0);
 
