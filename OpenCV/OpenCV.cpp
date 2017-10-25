b367f2cc1f10bb91e8b857b60650d75205d60d5e
XIIIvs <xiii_1994@wp.pl>
2017-10-25 13:13:28 +0200

    Refactor to Zajecia 1

--- OpenCV/OpenCV.cpp
+++ OpenCV/OpenCV.cpp
@@ -25,35 +25,6 @@ int main(int argc, char** argv)
 	namedWindow(window_name_1, WINDOW_AUTOSIZE); // Create a window for display.
 	imshow(window_name_1, image); // Show our image inside it.
 
-	Mat gray_image;
-	cvtColor(image, gray_image, CV_BGR2GRAY);
-
-
-	imwrite("../gray_lena.jpg", gray_image);
-	String window_name_2 = "Gray image";
-	namedWindow(window_name_2, WINDOW_AUTOSIZE);
-	imshow(window_name_2, gray_image);
-
-	waitKey(0);
-
-	imshow(window_name_1, image);
-	imshow(window_name_2, gray_image);
-
-	String window_name_3r = "Red image";
-	String window_name_3g = "Green image";
-	String window_name_3b = "Blue image";
-
-	namedWindow(window_name_3r, WINDOW_AUTOSIZE);
-	namedWindow(window_name_3g, WINDOW_AUTOSIZE);
-	namedWindow(window_name_3b, WINDOW_AUTOSIZE);
-
-	Mat color_layers[3];
-	split(image, color_layers);
-
-	imshow(window_name_3b, color_layers[0]);
-	imshow(window_name_3g, color_layers[1]);
-	imshow(window_name_3r, color_layers[2]);
-
 	waitKey(0);
 
 	return 0;
